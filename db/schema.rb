5c39f892c75241934bd539aab0b589c43a5c4403
Brandon Corbett <bmcorbet@valdosta.edu>
2018-09-02 08:18:43 -0400

    destroyed post scaffold, using news instead

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_09_01_224948) do
+ActiveRecord::Schema.define(version: 2018_09_02_121400) do
 
   create_table "friendly_id_slugs", force: :cascade do |t|
     t.string "slug", null: false
@@ -22,6 +22,16 @@ ActiveRecord::Schema.define(version: 2018_09_01_224948) do
     t.index ["slug", "sluggable_type"], name: "index_friendly_id_slugs_on_slug_and_sluggable_type"
     t.index ["sluggable_id"], name: "index_friendly_id_slugs_on_sluggable_id"
     t.index ["sluggable_type"], name: "index_friendly_id_slugs_on_sluggable_type"
+  end
+
+  create_table "news", force: :cascade do |t|
+    t.string "title"
+    t.text "body"
+    t.string "description"
+    t.string "slug"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["slug"], name: "index_news_on_slug", unique: true
   end
 
   create_table "posts", force: :cascade do |t|
