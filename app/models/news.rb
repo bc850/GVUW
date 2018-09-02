27abcfc59d44a8a9640eb8f590c6f6057d50820b
Brandon Corbett <bmcorbet@valdosta.edu>
2018-09-02 08:15:16 -0400

    extended friendly id in News and added title change method for slug

--- app/models/news.rb
+++ app/models/news.rb
@@ -1,2 +1,9 @@
 class News < ApplicationRecord
+  extend FriendlyId
+  friendly_id :title, use: :slugged
+
+  def should_generate_new_friendly_id?
+    # if you update the title, you want ot update the slug as well
+    title_changed?
+  end
 end
