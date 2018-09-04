bcdea10d33bc2e6a25f59fb3c2a8ec97f6b760a6
Brandon Corbett <bmcorbet@valdosta.edu>
2018-09-04 09:46:40 -0500

    changed redirect for author news controller create and update methods

--- app/controllers/author/news_controller.rb
+++ app/controllers/author/news_controller.rb
@@ -29,7 +29,7 @@ module Author
 
       respond_to do |format|
         if @news.save
-          format.html { redirect_to @news, notice: 'News was successfully created.' }
+          format.html { redirect_to author_news_path(@news), notice: 'News was successfully created.' }
           format.json { render :show, status: :created, location: @news }
         else
           format.html { render :new }
@@ -43,7 +43,7 @@ module Author
     def update
       respond_to do |format|
         if @news.update(news_params)
-          format.html { redirect_to @news, notice: 'News was successfully updated.' }
+          format.html { redirect_to author_news_path(@news), notice: 'News was successfully updated.' }
           format.json { render :show, status: :ok, location: @news }
         else
           format.html { render :edit }
