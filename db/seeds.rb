c1c8b4270ee504ddcd08740f0e07a86425e28049
Brandon Corbett <bmcorbet@valdosta.edu>
2018-09-04 15:46:15 -0500

    added some seeders for fake accounts and fake news *trump*

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,4 +6,56 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-Author.create!( :name => 'Brandon Corbett', :email => 'brandon.corbett@sgatechnology.com', :password => 'changeme', :password_confirmation => 'changeme' )
+Author.delete_all
+Author.create!(
+  :name => 'Brandon Corbett',
+  :email => 'brandon.corbett@sgatechnology.com',
+  :password => 'changeme',
+  :password_confirmation => 'changeme'
+)
+
+Author.create!(
+  :name => 'Leigh Anne Burns',
+  :email => 'laburns@sgatechnology.com',
+  :password => 'changeme',
+  :password_confirmation => 'changeme'
+)
+
+
+####### NEWS CREATION #######
+News.delete_all
+News.create!(
+  :title => 'Day of Caring',
+  :body => 'test',
+  :description => 'testing',
+  :slug => 'day-of-caring',
+  :banner_image_url => 'https://images.pexels.com/photos/1048256/pexels-photo-1048256.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260',
+  :author_id => 2,
+)
+
+News.create!(
+  :title => 'Mud Run',
+  :body => 'test',
+  :description => 'testing',
+  :slug => 'mud-run',
+  :banner_image_url => 'https://images.pexels.com/photos/33688/delicate-arch-night-stars-landscape.jpg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260',
+  :author_id => 2,
+)
+
+News.create!(
+  :title => 'United Way Golf Tournament',
+  :body => 'test',
+  :description => 'testing',
+  :slug => 'united-way-golf-tournament',
+  :banner_image_url => 'https://images.pexels.com/photos/730896/pexels-photo-730896.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260',
+  :author_id => 1,
+)
+
+News.create!(
+  :title => 'United Way Annual Campaign',
+  :body => 'test',
+  :description => 'testing',
+  :slug => 'united-way-annual-campaign',
+  :banner_image_url => 'https://images.pexels.com/photos/1093645/pexels-photo-1093645.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260',
+  :author_id => 1,
+)
