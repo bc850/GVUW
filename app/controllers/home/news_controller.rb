4eb45bf7de3380423f7a31a2caeff4d4c9347646
Brandon Corbett <bmcorbet@valdosta.edu>
2018-09-10 12:16:20 -0400

    changed news_controller index method to chain model scopes

--- app/controllers/home/news_controller.rb
+++ app/controllers/home/news_controller.rb
@@ -3,11 +3,7 @@ module Home
     # GET /news
     # GET /news.json
     def index
-      if params[:tag].present?
-        @news = news_prefix.most_recent.published.tagged_with(params[:tag]).paginate(:page => params[:page], :per_page => 3)
-      else
-        @news = news_prefix.most_recent.published.paginate(:page => params[:page], :per_page => 3)
-      end
+      @news = news_prefix.list_for(params[:page], params[:tag])
     end
 
     # GET /news/1
@@ -20,10 +16,6 @@ module Home
 
     def news_prefix
       News.published
-    end
-
-    def paginator
-      paginate(:page => params[:page], :per_page => 3)
     end
   end
 end
