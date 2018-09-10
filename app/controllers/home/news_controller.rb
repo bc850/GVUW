1c80489b13bd843ea3f634615f8399829055b991
Brandon Corbett <bmcorbet@valdosta.edu>
2018-09-10 12:04:54 -0400

    defined news_prefix private method for News.published

--- app/controllers/home/news_controller.rb
+++ app/controllers/home/news_controller.rb
@@ -4,16 +4,22 @@ module Home
     # GET /news.json
     def index
       if params[:tag].present?
-        @news = News.most_recent.published.tagged_with(params[:tag]).paginate(:page => params[:page], :per_page => 3)
+        @news = news_prefix.most_recent.published.tagged_with(params[:tag]).paginate(:page => params[:page], :per_page => 3)
       else
-        @news = News.most_recent.published.paginate(:page => params[:page], :per_page => 3)
+        @news = news_prefix.most_recent.published.paginate(:page => params[:page], :per_page => 3)
       end
     end
 
     # GET /news/1
     # GET /news/1.json
     def show
-      @news = News.published.friendly.find(params[:id])
+      @news = news_prefix.friendly.find(params[:id])
+    end
+
+    private
+
+    def news_prefix
+      News.published
     end
   end
 end
