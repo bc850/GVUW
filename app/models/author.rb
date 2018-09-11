1b684959fac8953a9d734eb51dd6ebb5e8d96818
Brandon Corbett <bmcorbet@valdosta.edu>
2018-09-11 01:15:45 -0400

    changed to display name in navbar, spacing between li elements

--- app/models/author.rb
+++ app/models/author.rb
@@ -30,4 +30,12 @@ class Author < ApplicationRecord
   def change_password(attrs)
     update(password: attrs[:new_password], password_confirmation: attrs[:new_password_confirmation])
   end
+
+  def display_name
+    if name.present?
+      name
+    else
+      "Author"
+    end
+  end
 end
