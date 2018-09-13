8639c1c47c7b52ae77d209f4a55634734d864cd9
Brandon Corbett <bmcorbet@valdosta.edu>
2018-09-13 07:17:31 -0500

    added some event seeders

--- db/seeds.rb
+++ db/seeds.rb
@@ -63,3 +63,61 @@ News.create!(
   :author_id => 1,
   :published => true,
 )
+
+####### EVENTS CREATION #######
+Event.delete_all
+Event.create!(
+  :title => 'Possum Creek Mud Run',
+  :body => 'Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old. Richard McClintock, a Latin professor at Hampden-Sydney College in Virginia, looked up one of the more obscure Latin words, consectetur, from a Lorem Ipsum passage, and going through the cites of the word in classical literature, discovered the undoubtable source. Lorem Ipsum comes from sections 1.10.32 and 1.10.33 of "de Finibus Bonorum et Malorum" (The Extremes of Good and Evil) by Cicero, written in 45 BC. This book is a treatise on the theory of ethics, very popular during the Renaissance. The first line of Lorem Ipsum, "Lorem ipsum dolor sit amet..", comes from a line in section 1.10.32.
+
+The standard chunk of Lorem Ipsum used since the 1500s is reproduced below for those interested. Sections 1.10.32 and 1.10.33 from "de Finibus Bonorum et Malorum" by Cicero are also reproduced in their exact original form, accompanied by English versions from the 1914 translation by H. Rackham.',
+  :description => 'This is the Possum Creek Mud Run near Ray City, GA. It will happen and be awesome. Many Moody AFB personnel will probably be there.',
+  :slug => 'possum-creek-mud-run',
+  :banner_image_url => 'https://images.pexels.com/photos/914128/pexels-photo-914128.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260',
+  :author_id => '1',
+  :published => 'true',
+  :featured => 'true',
+  :published_at => '2018-09-12',
+)
+
+Event.create!(
+  :title => 'SGMC Blood Drive',
+  :body => 'Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old. Richard McClintock, a Latin professor at Hampden-Sydney College in Virginia, looked up one of the more obscure Latin words, consectetur, from a Lorem Ipsum passage, and going through the cites of the word in classical literature, discovered the undoubtable source. Lorem Ipsum comes from sections 1.10.32 and 1.10.33 of "de Finibus Bonorum et Malorum" (The Extremes of Good and Evil) by Cicero, written in 45 BC. This book is a treatise on the theory of ethics, very popular during the Renaissance. The first line of Lorem Ipsum, "Lorem ipsum dolor sit amet..", comes from a line in section 1.10.32.
+
+The standard chunk of Lorem Ipsum used since the 1500s is reproduced below for those interested. Sections 1.10.32 and 1.10.33 from "de Finibus Bonorum et Malorum" by Cicero are also reproduced in their exact original form, accompanied by English versions from the 1914 translation by H. Rackham.',
+  :description => 'This is the Possum Creek Mud Run near Ray City, GA. It will happen and be awesome. Many Moody AFB personnel will probably be there.',
+  :slug => 'sgmc-blood-drive',
+  :banner_image_url => 'https://images.pexels.com/photos/1064566/pexels-photo-1064566.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260',
+  :author_id => '1',
+  :published => 'true',
+  :featured => 'true',
+  :published_at => '2018-09-11',
+)
+
+Event.create!(
+  :title => 'Hawaiian BBQ Sandwich Sale',
+  :body => 'Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old. Richard McClintock, a Latin professor at Hampden-Sydney College in Virginia, looked up one of the more obscure Latin words, consectetur, from a Lorem Ipsum passage, and going through the cites of the word in classical literature, discovered the undoubtable source. Lorem Ipsum comes from sections 1.10.32 and 1.10.33 of "de Finibus Bonorum et Malorum" (The Extremes of Good and Evil) by Cicero, written in 45 BC. This book is a treatise on the theory of ethics, very popular during the Renaissance. The first line of Lorem Ipsum, "Lorem ipsum dolor sit amet..", comes from a line in section 1.10.32.
+
+The standard chunk of Lorem Ipsum used since the 1500s is reproduced below for those interested. Sections 1.10.32 and 1.10.33 from "de Finibus Bonorum et Malorum" by Cicero are also reproduced in their exact original form, accompanied by English versions from the 1914 translation by H. Rackham.',
+  :description => 'This is the Possum Creek Mud Run near Ray City, GA. It will happen and be awesome. Many Moody AFB personnel will probably be there.',
+  :slug => 'hawaiian-bbq-sandwich-sale',
+  :banner_image_url => 'https://images.pexels.com/photos/951382/pexels-photo-951382.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260',
+  :author_id => '1',
+  :published => 'true',
+  :featured => 'true',
+  :published_at => '2018-09-02',
+)
+
+Event.create!(
+  :title => 'Habitat for Humanity Birdhouse Auction',
+  :body => 'Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old. Richard McClintock, a Latin professor at Hampden-Sydney College in Virginia, looked up one of the more obscure Latin words, consectetur, from a Lorem Ipsum passage, and going through the cites of the word in classical literature, discovered the undoubtable source. Lorem Ipsum comes from sections 1.10.32 and 1.10.33 of "de Finibus Bonorum et Malorum" (The Extremes of Good and Evil) by Cicero, written in 45 BC. This book is a treatise on the theory of ethics, very popular during the Renaissance. The first line of Lorem Ipsum, "Lorem ipsum dolor sit amet..", comes from a line in section 1.10.32.
+
+The standard chunk of Lorem Ipsum used since the 1500s is reproduced below for those interested. Sections 1.10.32 and 1.10.33 from "de Finibus Bonorum et Malorum" by Cicero are also reproduced in their exact original form, accompanied by English versions from the 1914 translation by H. Rackham.',
+  :description => 'This is the Possum Creek Mud Run near Ray City, GA. It will happen and be awesome. Many Moody AFB personnel will probably be there.',
+  :slug => 'habitat-for-humanity-birdhouse-auction',
+  :banner_image_url => 'https://images.pexels.com/photos/675764/pexels-photo-675764.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260',
+  :author_id => '1',
+  :published => 'true',
+  :featured => 'true',
+  :published_at => '2018-08-24',
+)
