770a6f2bdcf926d7f3c976ff99b677d4f6452428
Brandon Corbett <bmcorbet@valdosta.edu>
2018-09-12 23:10:23 -0500

    set_event method event chain -> events chain

--- app/controllers/authors/events_controller.rb
+++ app/controllers/authors/events_controller.rb
@@ -70,7 +70,7 @@ module Authors
     private
       # Use callbacks to share common setup or constraints between actions.
       def set_event
-        @event = current_author.event.friendly.find(params[:id])
+        @event = current_author.events.friendly.find(params[:id])
       end
 
       # Never trust parameters from the scary internet, only allow the white list through.
