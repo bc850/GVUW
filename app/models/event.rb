3bf16eb92725f605efcedc9de84bb8e69f7b7c7f
Brandon Corbett <bmcorbet@valdosta.edu>
2018-09-12 22:03:16 -0500

    added acts_as_taggable, extended FriendlyId to events model

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,3 +1,10 @@
 class Event < ApplicationRecord
   has_many :registrations
+
+  acts_as_taggable # Alias for acts_as_taggable_on :tags
+
+  extend FriendlyId
+  friendly_id :title, use: :slugged
+
+  
 end
