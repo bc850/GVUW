5ad279dcc2c96d404f85838eaf36c16865ae2e50
Brandon Corbett <bmcorbet@valdosta.edu>
2018-09-15 02:53:39 -0400

    added authors/campaigns_controller.rb, added methods

---
+++ app/controllers/authors/campaigns_controller.rb
@@ -0,0 +1,72 @@
+module Authors
+  class CampaignsController < AuthorController
+    before_action :set_event, only: [:show, :edit, :update, :destroy, :publish, :unpublish]
+
+    def index
+      @campaigns = current_author.campaigns.most_recent.list_for(params[:page], params[:tag])
+    end
+
+    def new
+      @campaigns = current_author.campaigns.new
+    end
+
+    def show
+    end
+
+    def publish
+      @campaign.publish
+      redirect_to authors_campaigns_path
+    end
+
+    def unpublish
+      @campaign.unpublish
+      redirect_to authors_campaigns_path
+    end
+
+    def create
+      @campaign = current_author.campaigns.new(campaign_params)
+
+      respond_to do |format|
+        if @campaign.save
+          format.html { redirect_to authors_campaign_path(@campaign), notice: 'Campaign post was successfully created.' }
+          format.json { render :show, status: :created, location: @campaign }
+        else
+          format.html { render :new }
+          format.json { render json: @campaign.errors, status: :unprocessable_entity }
+        end
+      end
+    end
+
+    def update
+      respond_to do |format|
+        if @campaign.update(campaign_params)
+          format.html { redirect_to authors_campaign_path(@campaign), notice: 'Campaigns post was successfully updated.' }
+          format.json { render :show, status: :ok, location: @campaign }
+        else
+          format.html { render :edit }
+          format.json { render json: @campaign.errors, status: :unprocessable_entity }
+        end
+      end
+    end
+
+    def destroy
+      @campaign.destroy
+      respond_to do |format|
+        format.html { redirect_to authors_campaigns_path, notice: 'Campaign post was successfully destroyed.' }
+        format.json { head :no_content }
+      end
+    end
+
+    private
+      # Use callbacks to share common setup or constraints between actions.
+      def set_campaign
+        @campaign = current_author.campaigns.friendly.find(params[:id])
+      end
+
+      # Never trust parameters from the scary internet, only allow the white list through.
+      def campaign_params
+        params.require(:event).permit(:year, :total, :percentage, :body, :author_id, :slug, :tag_list, :published, :published_at)
+      end
+
+  end
+end
