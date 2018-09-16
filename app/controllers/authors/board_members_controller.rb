beebc54d8def87a24a95c338d4565825f1a7e121
Brandon Corbett <bmcorbet@valdosta.edu>
2018-09-16 12:21:47 -0400

    changed variable names to @board_member(s)

--- app/controllers/authors/board_members_controller.rb
+++ app/controllers/authors/board_members_controller.rb
@@ -3,43 +3,43 @@ module Authors
     before_action :set_board_member, only: [:show, :edit, :update, :destroy]
 
     def index
-      @members = BoardMember.alphabetical
+      @board_members = BoardMember.alphabetical
     end
 
     def new
-      @member = BoardMember.new
+      @board_member = BoardMember.new
     end
 
     def create
-      @member = BoardMember.new(board_member_params)
+      @board_member = BoardMember.new(board_member_params)
 
       respond_to do |format|
-        if @member.save
+        if @board_member.save
           format.html { redirect_to authors_board_members_path, notice: 'Board Member was successfully created.' }
-          format.json { render :show, status: :created, location: @member }
+          format.json { render :show, status: :created, location: @board_member }
         else
           format.html { render :new }
-          format.json { render json: @member.errors, status: :unprocessable_entity }
+          format.json { render json: @board_member.errors, status: :unprocessable_entity }
         end
       end
     end
 
     def update
       respond_to do |format|
-        if @member.update(board_member_params)
-          format.html { redirect_to edit_authors_board_member_path(@member), notice: 'Board Member was successfully updated.' }
-          format.json { render :show, status: :ok, location: @member }
+        if @board_member.update(board_member_params)
+          format.html { redirect_to edit_authors_board_member_path(@board_member), notice: 'Board Member was successfully updated.' }
+          format.json { render :show, status: :ok, location: @board_member }
         else
           format.html { render :edit }
-          format.json { render json: @member.errors, status: :unprocessable_entity }
+          format.json { render json: @board_member.errors, status: :unprocessable_entity }
         end
       end
     end
 
     def destroy
-      @member.destroy
+      @board_member.destroy
       respond_to do |format|
-        format.html { redirect_to authors_campaigns_path, notice: 'Board Member was successfully destroyed.' }
+        format.html { redirect_to authors_board_members_path, notice: 'Board Member was successfully destroyed.' }
         format.json { head :no_content }
       end
     end
@@ -47,7 +47,7 @@ module Authors
     private
 
     def set_board_member
-      @member = BoardMember.find(params[:id])
+      @board_member = BoardMember.find(params[:id])
     end
 
     def board_member_params
