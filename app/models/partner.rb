761499486557dab6f407f7d9a26d6bef2e672e7a
Brandon Corbett <bmcorbet@valdosta.edu>
2018-09-19 16:23:10 -0500

    added scopes and methods to partner model, updated form,partner,edit&new

--- app/models/partner.rb
+++ app/models/partner.rb
@@ -15,4 +15,42 @@
 #
 
 class Partner < ApplicationRecord
+  belongs_to :author
+
+  acts_as_taggable # Alias for acts_as_taggable_on :tags
+
+  extend FriendlyId
+  friendly_id :title, use: :slugged
+
+  PER_PAGE = 3
+
+  scope :alphabetical, -> { order(name: :asc) }
+  scope :published, -> { where(published: true) }
+  scope :recent_paginate, -> (page) { most_recent.paginate(page: page, per_page: PER_PAGE) }
+  scope :with_tag, -> (tag) { tagged_with(tag) if tag.present? }
+
+  scope :list_for, -> (page, tag) do
+    recent_paginate(page).with_tag(tag)
+  end
+
+  def should_generate_new_friendly_id?
+    # if you update the title, you want to update the slug as well
+    name_changed?
+  end
+
+  def display_day_published
+    if published_at.present?
+      "Published #{published_at.strftime('%-b %-d, %Y')}"
+    else
+      "Not published yet."
+    end
+  end
+
+  def publish
+    update(published: true, published_at: Time.now)
+  end
+
+  def unpublish
+    update(published: false, published_at: nil)
+  end
 end
