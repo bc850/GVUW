f4fe4dc4fca3ea5dcc7b54f0225d167449a86fde
Brandon Corbett <bmcorbet@valdosta.edu>
2018-09-20 10:50:25 -0500

    added home/partners/show.html.erb

--- app/controllers/home/partners_controller.rb
+++ app/controllers/home/partners_controller.rb
@@ -5,6 +5,10 @@ module Home
       @partners = partner_prefix.list_for(params[:page], params[:tag])
     end
 
+    def show
+      @partner = partner_prefix.friendly.find(params[:id])
+    end
+
     private
 
     def partner_prefix
