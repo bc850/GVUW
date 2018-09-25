481392d4035bbfa553cb2d5220bd812b15396657
Brandon Corbett <bmcorbet@valdosta.edu>
2018-09-24 23:03:05 -0500

    edited set_organization to .first

--- app/controllers/authors/organizations_controller.rb
+++ app/controllers/authors/organizations_controller.rb
@@ -1,15 +1,11 @@
 module Authors
   class OrganizationsController < AuthorController
-    before_action :set_organization, only: [:show, :edit, :update]
-
-    def show
-      
-    end
+    before_action :set_organization, only: [:edit, :update]
 
     def update
       respond_to do |format|
         if @organization.update(organization_params)
-          format.html { redirect_to authors_organization_path(@organization), notice: 'Organization was successfully updated.' }
+          format.html { redirect_to edit_authors_organization_path(@organization), notice: 'Organization was successfully updated.' }
           format.json { render :show, status: :ok, location: @organization }
         else
           format.html { render :edit }
@@ -21,7 +17,7 @@ module Authors
     private
 
     def set_organization
-      @organization = Organization.find(params[:id])
+      @organization = Organization.first
     end
 
     def organization_params
