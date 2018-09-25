9520f78bf6ab9dec333f94abd9e7391456dfbfe6
Brandon Corbett <bmcorbet@valdosta.edu>
2018-09-24 21:54:35 -0500

    annotated the organization model

--- test/models/organization_test.rb
+++ test/models/organization_test.rb
@@ -1,3 +1,23 @@
+# == Schema Information
+#
+# Table name: organizations
+#
+#  id         :bigint(8)        not null, primary key
+#  name       :string
+#  address    :string
+#  city       :string
+#  state      :string
+#  zip        :string
+#  phone      :string
+#  email      :string
+#  latitude   :float
+#  longitude  :float
+#  map_api    :string
+#  logo       :string
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
+
 require 'test_helper'
 
 class OrganizationTest < ActiveSupport::TestCase
