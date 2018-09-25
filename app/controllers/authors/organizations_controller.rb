003b1ed90b450e35bfca2c8eee0b304bad4fd7af
Brandon Corbett <bmcorbet@valdosta.edu>
2018-09-24 23:03:35 -0500

    removed blank space

--- app/controllers/authors/organizations_controller.rb
+++ app/controllers/authors/organizations_controller.rb
@@ -23,6 +23,5 @@ module Authors
     def organization_params
       params.require(:organization).permit(:name, :address, :city, :state, :zip, :phone, :email, :latitude, :longitude, :map_api, :logo)
     end
-
   end
 end
