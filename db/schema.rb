f701b4a81863badec38a84d6e494e056f5dcaf12
Brandon Corbett <bmcorbet@valdosta.edu>
2018-09-28 15:35:58 -0400

    added mailchimp_api to organization model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_09_25_025236) do
+ActiveRecord::Schema.define(version: 2018_09_28_193515) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -119,6 +119,7 @@ ActiveRecord::Schema.define(version: 2018_09_25_025236) do
     t.string "logo"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "mailchimp_api"
   end
 
   create_table "partners", force: :cascade do |t|
--- test/models/organization_test.rb
+++ test/models/organization_test.rb
@@ -2,20 +2,21 @@
 #
 # Table name: organizations
 #
-#  id         :bigint(8)        not null, primary key
-#  name       :string
-#  address    :string
-#  city       :string
-#  state      :string
-#  zip        :string
-#  phone      :string
-#  email      :string
-#  latitude   :float
-#  longitude  :float
-#  map_api    :string
-#  logo       :string
-#  created_at :datetime         not null
-#  updated_at :datetime         not null
+#  id            :bigint(8)        not null, primary key
+#  name          :string
+#  address       :string
+#  city          :string
+#  state         :string
+#  zip           :string
+#  phone         :string
+#  email         :string
+#  latitude      :float
+#  longitude     :float
+#  map_api       :string
+#  logo          :string
+#  created_at    :datetime         not null
+#  updated_at    :datetime         not null
+#  mailchimp_api :string
 #
 
 require 'test_helper'
