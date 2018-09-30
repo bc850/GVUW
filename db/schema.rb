4a432a7c0283b6a0bd0fef210dae8778bc3745a8
Brandon Corbett <bmcorbet@valdosta.edu>
2018-09-30 15:56:40 -0400

    updates to statement partial in our work, added date and time to events

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_09_29_155846) do
+ActiveRecord::Schema.define(version: 2018_09_30_195553) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -75,6 +75,9 @@ ActiveRecord::Schema.define(version: 2018_09_29_155846) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.boolean "registerable"
+    t.date "event_date"
+    t.time "start_time"
+    t.time "end_time"
   end
 
   create_table "friendly_id_slugs", id: :serial, force: :cascade do |t|
