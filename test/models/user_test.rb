d83bcdaba50359a743edbc87ab4bd56af9945e70
Brandon Corbett <bmcorbet@valdosta.edu>
2018-10-01 22:34:15 -0400

    annotated user model

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -1,3 +1,27 @@
+# == Schema Information
+#
+# Table name: users
+#
+#  id                     :bigint(8)        not null, primary key
+#  email                  :string           default(""), not null
+#  encrypted_password     :string           default(""), not null
+#  reset_password_token   :string
+#  reset_password_sent_at :datetime
+#  remember_created_at    :datetime
+#  sign_in_count          :integer          default(0), not null
+#  current_sign_in_at     :datetime
+#  last_sign_in_at        :datetime
+#  current_sign_in_ip     :inet
+#  last_sign_in_ip        :inet
+#  created_at             :datetime         not null
+#  updated_at             :datetime         not null
+#  superadmin_role        :boolean          default(FALSE)
+#  admin_role             :boolean          default(FALSE)
+#  author_role            :boolean          default(FALSE)
+#  board_member_role      :boolean          default(FALSE)
+#  user_role              :boolean          default(TRUE)
+#
+
 require 'test_helper'
 
 class UserTest < ActiveSupport::TestCase
