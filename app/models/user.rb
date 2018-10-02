7699a08df568ae4fbd24ec6d6cbdbdbbf83ee975
Brandon Corbett <bmcorbet@valdosta.edu>
2018-10-01 22:37:37 -0400

    added example abilities in user.rb

--- app/models/user.rb
+++ app/models/user.rb
@@ -27,4 +27,19 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
+
+  include CanCan::Ability
+
+  def initialize(user)
+    # Define abilities for the passed in user here. For example:
+
+    user ||= User.new # guest user (not logged in)
+    if user.superadmin_role?
+      can :manage, :all
+    end
+    if user.admin_role?
+      can :manage, User
+    end
+
+  end
 end
